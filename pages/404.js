0b4a4516d449f454ee645e6610421983eac2a869
alpliar <al.pliar@gmail.com>
2021-01-30 16:45:44 +0100

    style: update 404

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function Custom404() {
             <Head>{siteTitle} - 404</Head>
 
             <div className="md:container md:mx-auto pb-32 px-4 text-center text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">
-                <h1 className=" text-gray-900">404</h1>
+                <h1 className="text-gray-900">404</h1>
                 <span className="block text-indigo-600 xl:inline">Page not found</span>
             </div>
         </Layout>
