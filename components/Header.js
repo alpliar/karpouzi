fc2cb9789b1d4b4a989f1559d585cce8de4d941a
alpliar <al.pliar@gmail.com>
2021-01-31 00:10:52 +0100

    style: fix comestic issues

--- components/Header.js
+++ components/Header.js
@@ -6,18 +6,8 @@ const Header = ({ siteTitle }) => {
     return (
         <div className="relative overflow-hidden">
             <div className="max-w-7xl mx-auto">
-                <div
-                    className={`relative z-10 pb-8 bg-white dark:bg-gray-800 sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32`}>
-                    <svg
-                        className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-                        fill="#1F2937"
-                        viewBox="0 0 100 100"
-                        preserveAspectRatio="none"
-                        aria-hidden="true">
-                        <polygon points="50,0 100,0 50,100 0,100" />
-                    </svg>
-
-                    <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                <div className="pb-8">
+                    <div className=" pt-6 px-4 sm:px-6 lg:px-8">
                         <nav
                             className="relative flex items-center justify-between sm:h-10 lg:justify-start"
                             aria-label="Global">
@@ -27,11 +17,6 @@ const Header = ({ siteTitle }) => {
                                         <a className="text-xl tracking-tight font-extrabold text-indigo-600 sm:text-2xl md:text-3xl">
                                             {siteTitle}
                                         </a>
-                                        {/* <img
-                                            alt="fzafzafzaf"
-                                            className="h-8 w-auto sm:h-10"
-                                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                        /> */}
                                     </Link>
                                 </div>
                             </div>
