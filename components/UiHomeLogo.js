9e361e89e863302bd1be7e1ef2bacd921313e847
alpliar <al.pliar@gmail.com>
2021-02-01 23:23:39 +0100

    fix logo

--- components/UiHomeLogo.js
+++ components/UiHomeLogo.js
@@ -5,16 +5,17 @@ import { Link as UiLink, Text } from 'theme-ui';
 const UiHomeLogo = ({ siteTitle }) => {
     return (
         <Link href="/">
-            <UiLink color="onPrimary">
-                <span as="span" role="img" aria-label="Yarn">
-                    🧶
-                </span>
+            <UiLink href="/" color="onPrimary">
                 <Text
                     sx={{
-                        fontSize: 3,
-                        fontWeight: 'bold'
+                        fontSize: 5,
+                        fontWeight: 'bold',
+                        display: 'inline'
                     }}
                     color="onPrimary">
+                    <span as="span" role="img" aria-label="Yarn">
+                        🧶
+                    </span>
                     {siteTitle}
                 </Text>
             </UiLink>
