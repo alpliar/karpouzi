b671f6054ba8d1e5d94b6af4691812cbab8ef642
alpliar <al.pliar@gmail.com>
2021-02-04 13:54:19 +0100

    a11y: fixed header background/text contrast

--- components/Header.js
+++ components/Header.js
@@ -21,7 +21,7 @@ const Header = ({ siteTitle }) => {
             justify="space-between"
             wrap="wrap"
             padding="1.5rem"
-            bg="teal.500"
+            bg="teal.600"
             color="white">
             <Flex align="center" mr={5}>
                 <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
