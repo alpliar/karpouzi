a2ff077865aa1e645146c356dd2f908405f52383
alpliar <al.pliar@gmail.com>
2021-02-05 20:31:50 +0100

    style: bs homepage

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,86 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
-import LatestsPosts from '../components/LatestsPosts';
-import { getSortedPostsData } from '../lib/posts';
 import { PropTypes } from 'prop-types';
 
-import { Box, Button, Container, Divider, Heading, useToast } from '@chakra-ui/react';
+import {
+    Box,
+    Container,
+    Divider,
+    Heading,
+    Stat,
+    StatGroup,
+    StatHelpText,
+    StatArrow,
+    StatLabel,
+    StatNumber,
+    Table,
+    TableCaption,
+    Tbody,
+    Tfoot,
+    Tr,
+    Td,
+    Th,
+    Thead
+} from '@chakra-ui/react';
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
-    return {
-        props: {
-            allPostsData
-        }
-    };
-}
+const Stats = (
+    <StatGroup>
+        <Stat>
+            <StatLabel>Sent</StatLabel>
+            <StatNumber>345,670</StatNumber>
+            <StatHelpText>
+                <StatArrow type="increase" />
+                23.36%
+            </StatHelpText>
+        </Stat>
 
-function ToastExample() {
-    const toast = useToast();
-    return (
-        <Button
-            onClick={() =>
-                toast({
-                    title: 'Account created.',
-                    description: "We've created your account for you.",
-                    status: 'success',
-                    duration: 9000,
-                    isClosable: true
-                })
-            }>
-            Show Toast
-        </Button>
-    );
-}
-
-export default function Home({ allPostsData }) {
+        <Stat>
+            <StatLabel>Clicked</StatLabel>
+            <StatNumber>45</StatNumber>
+            <StatHelpText>
+                <StatArrow type="decrease" />
+                9.05%
+            </StatHelpText>
+        </Stat>
+    </StatGroup>
+);
+const TableStat = (
+    <Table variant="striped">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+            <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+            </Tr>
+        </Thead>
+        <Tbody>
+            <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+                <Td isNumeric>25.4</Td>
+            </Tr>
+            <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+                <Td isNumeric>30.48</Td>
+            </Tr>
+            <Tr>
+                <Td>yards</Td>
+                <Td>metres (m)</Td>
+                <Td isNumeric>0.91444</Td>
+            </Tr>
+        </Tbody>
+        <Tfoot>
+            <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+            </Tr>
+        </Tfoot>
+    </Table>
+);
+export default function Home() {
     // const [showModal, setShowModal] = useState(false);
 
     return (
@@ -41,7 +88,6 @@ export default function Home({ allPostsData }) {
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-
             <Container p={4} maxW="4xl">
                 <Box>
                     <Heading>Hi, welcome !</Heading>
@@ -51,12 +97,13 @@ export default function Home({ allPostsData }) {
             <Divider />
 
             <Container p={4} maxW="4xl">
-                <Box>
-                    <LatestsPosts posts={allPostsData} />
-                </Box>
-                <Box>
-                    <ToastExample />
-                </Box>
+                {Stats}
+            </Container>
+
+            <Divider />
+
+            <Container p={4} maxW="4xl">
+                {TableStat}
             </Container>
         </Layout>
     );
