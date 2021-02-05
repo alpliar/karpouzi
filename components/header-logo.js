3737bfeb2bc341bfa8a16c0d451f0d954b36385e
alpliar <al.pliar@gmail.com>
2021-02-05 18:09:13 +0100

    fix: headerlogo syntax

--- components/header-logo.js
+++ components/header-logo.js
@@ -3,16 +3,18 @@ import { Heading, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const HeaderLogo = ({ siteTitle }) => {
-    <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
-        <NextLink href="/">
-            <Link _hover={{ textDecor: 'none' }} alt="go to home">
-                <span role="img" aria-label="Watermelon">
-                    🍉
-                </span>
-                {siteTitle}
-            </Link>
-        </NextLink>
-    </Heading>;
+    return (
+        <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
+            <NextLink href="/">
+                <Link _hover={{ textDecor: 'none' }} alt="go to home">
+                    <span role="img" aria-label="Watermelon">
+                        🍉
+                    </span>
+                    {siteTitle}
+                </Link>
+            </NextLink>
+        </Heading>
+    );
 };
 export default HeaderLogo;
 
