8153d28d69c99a08674e18f571fc2f60652146f9
alpliar <al.pliar@gmail.com>
2021-02-25 16:27:49 +0100

    feature: add to cart toast

--- components/addToCart.js
+++ components/addToCart.js
@@ -1,14 +1,22 @@
 import PropTypes from 'prop-types';
-import { Button } from '@chakra-ui/react';
+import { Button, useToast } from '@chakra-ui/react';
 import { useDispatch } from 'react-redux';
 import { ADD_TO_CART } from '../actions/shop';
 
 const AddToCart = ({ slug, quantity = 1 }) => {
     const dispatch = useDispatch();
+    const toast = useToast();
 
     const handleClick = () => {
-        //console.log(slug);
         dispatch({ type: ADD_TO_CART, payload: slug });
+        toast({
+            title: `Item added to cart`,
+            description: `${slug}`,
+            status: 'success',
+            position: 'bottom-right',
+            duration: 2000,
+            isClosable: true
+        });
     };
 
     return <Button onClick={handleClick}>Add to cart</Button>;
