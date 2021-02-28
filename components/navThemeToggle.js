235c868c6fabc45132313b2ebdacd4249a3c667e
alpliar <al.pliar@gmail.com>
2021-03-01 00:24:49 +0100

    fix : Module not found: Error: Can't resolve '../components/NavThemeToggle'

--- components/navThemeToggle.js
+++ components/navThemeToggle.js
@@ -2,8 +2,10 @@ import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { useColorMode, useColorModeValue } from '@chakra-ui/react';
 import NavButton from './navButton';
 
-export default function NavThemeToggle() {
+const NavThemeToggle = () => {
     const { toggleColorMode: toggleMode } = useColorMode();
     const ToggleIcon = useColorModeValue(MoonIcon, SunIcon);
     return <NavButton icon={<ToggleIcon />} ariaLabel={'Toggle theme'} handleClick={toggleMode} />;
-}
+};
+
+export default NavThemeToggle;
