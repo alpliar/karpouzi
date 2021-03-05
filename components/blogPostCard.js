c5be05dc86aad5332fd5d4dd7fa78f03ba82e598
alpliar <al.pliar@gmail.com>
2021-03-05 12:29:15 +0100

    feature: limit postcard text length to 3 lines

--- components/blogPostCard.js
+++ components/blogPostCard.js
@@ -54,7 +54,7 @@ const BlogPostCard = ({ title, date, slug }) => {
                                 <LinkOverlay>{title}</LinkOverlay>
                             </Link>
                         </Heading>
-                        <Text color={'gray.500'}>
+                        <Text color={'gray.500'} noOfLines="3">
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
                             eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
                             voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
