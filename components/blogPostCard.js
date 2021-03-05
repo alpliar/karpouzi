e6bf83ce771d56ce563e8c2705bf0e79fc8860d7
alpliar <al.pliar@gmail.com>
2021-03-05 12:31:52 +0100

    fix: blog post ssr image

--- components/blogPostCard.js
+++ components/blogPostCard.js
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types';
-import Image from 'next/image';
-import Link from 'next/link';
 import {
+    Avatar,
     Box,
     Center,
     Heading,
-    Text,
-    Stack,
-    Avatar,
-    useColorModeValue,
+    Img,
     LinkBox,
-    LinkOverlay
+    LinkOverlay,
+    Stack,
+    Text,
+    useColorModeValue
 } from '@chakra-ui/react';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
 import karpouziAvatar from '../public/icon-48x48.png';
 
 const BlogPostCard = ({ title, date, slug }) => {
@@ -27,7 +27,7 @@ const BlogPostCard = ({ title, date, slug }) => {
                     p={6}
                     overflow={'hidden'}>
                     <Box h={'210px'} bg={'gray.100'} mt={-6} mx={-6} mb={6} pos={'relative'}>
-                        <Image
+                        <Img
                             src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
                             layout={'fill'}
                         />
