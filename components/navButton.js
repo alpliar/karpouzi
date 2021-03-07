c627ddbd58b2784ccd3feb4a354dba651e122f22
alpliar <al.pliar@gmail.com>
2021-03-08 00:21:03 +0100

    fix: bigger buttons on small devices (touchscreen)

--- components/navButton.js
+++ components/navButton.js
@@ -11,7 +11,7 @@ const NavButton = ({ icon, label, handleClick, isPrimary, compact }) => {
     const darkColor = 'white';
     const color = useColorModeValue(lightColor, darkColor);
     // useBreakPoint value is not available instantly, need a 'sm' fallback in order to prevent buttons resize on load
-    const size = 'sm' || useBreakpointValue({ base: 'sm', md: 'md' });
+    const size = useBreakpointValue({ base: 'md', md: 'sm' }) || 'md';
 
     if (compact === true) {
         return (
