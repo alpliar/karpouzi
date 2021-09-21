e86b8315f8d8f094f305c5a33085c564e0dd9b82
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-09-21 13:43:17 +0200

    feature: add copyright mention to footer

--- components/pageFooter.tsx
+++ components/pageFooter.tsx
@@ -1,16 +1,20 @@
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, Stack, StackItem } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import BackToHome from './backToHome';
 
 const Footer = () => {
     const router = useRouter();
     const isHome = router.pathname === '/';
+    const copyrightMention = `© Karpouzi ${new Date().getFullYear()}`;
 
     return (
         <Box bgColor="blackAlpha.100" mt={8}>
             <hr />
             <Container as="footer" p={4} textAlign="center">
-                {!isHome && <BackToHome />}
+                <Stack>
+                    <StackItem>{!isHome && <BackToHome />}</StackItem>
+                    <StackItem>{copyrightMention}</StackItem>
+                </Stack>
             </Container>
         </Box>
     );
