0e7d54d2d7d17e198e2d2376c0d497b7418bd952
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-10-31 17:01:15 +0100

    chore: remove dead code

--- cypress/integration/homepage/locales.spec.ts
+++ cypress/integration/homepage/locales.spec.ts
@@ -34,35 +34,6 @@ describe('Locale', () => {
                 .screenshot();
         });
     });
-    // it('Set locale to EN', () => {
-    //     cy.get('button[e2e=localeToggle]').click();
-    //     cy.get('[e2e=localeMenu]').should('be.visible').contains('English').click();
-    //     cy.get('h2').first().contains('Welcome').screenshot();
-    // });
-    it('Set locale to FR', () => {
-        cy.get('button[e2e=localeToggle]').should('be.visible').screenshot().click();
-        cy.get('[e2e=localeMenu]')
-            .screenshot()
-            .should('be.visible')
-            .contains('Français')
-            .should('exist')
-            .screenshot()
-            .click({ force: true });
-        cy.location('pathname').should('eq', '/fr');
-        cy.get('h2').first().contains('Bienvenue').screenshot();
-    });
-    // it('Set locale to ES', () => {
-    //     cy.get('button[e2e=localeToggle]').click();
-    //     cy.get('[e2e=localeMenu]').should('be.visible').contains('Español').click();
-    //     // cy.location().should((loc) => expect(loc.pathname).to.eq('/es'));
-    //     cy.get('h2').first().contains('Bienvenido').screenshot();
-    // });
-    // it('Set locale to GR', () => {
-    //     cy.get('button[e2e=localeToggle]').click();
-    //     cy.get('[e2e=localeMenu]').should('be.visible').contains('Ελληνικά').click();
-    //     // cy.location().should((loc) => expect(loc.pathname).to.eq('/gr'));
-    //     cy.get('h2').first().contains('καλώς').screenshot();
-    // });
 });
 
 export {};
