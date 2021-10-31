62f4e9aa4939e93caa3e62ab6d36b232c227844c
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-10-31 12:39:40 +0100

    update locale tests

--- components/navLocaleSelector.tsx
+++ components/navLocaleSelector.tsx
@@ -1,12 +1,14 @@
 import { Button, IconButton } from '@chakra-ui/button';
 import { useColorModeValue } from '@chakra-ui/color-mode';
 import { Image } from '@chakra-ui/image';
+import { LinkBox, LinkOverlay, Stack } from '@chakra-ui/layout';
 import { useBreakpointValue } from '@chakra-ui/media-query';
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { FaFlag } from 'react-icons/fa';
 import getFlagEmoji from '../utils/flags';
+import Link from './link';
 
 const NavLocaleSelector = ({ compact }) => {
     const router = useRouter();
@@ -27,6 +29,7 @@ const NavLocaleSelector = ({ compact }) => {
     return (
         <Menu>
             <MenuButton
+                e2e="localeToggle"
                 size={size}
                 color="white"
                 colorScheme={colorScheme}
@@ -34,27 +37,21 @@ const NavLocaleSelector = ({ compact }) => {
                 icon={compact === true && <FaFlag />}>
                 {compact === false && 'Language'}
             </MenuButton>
-            <MenuList color={useColorModeValue('black', 'white')}>
+            <MenuList paddingX="2" e2e="localeMenu" color={useColorModeValue('black', 'white')}>
                 {router.locales.map((locale) => {
                     const localeName = localesInfos[locale];
                     return (
-                        <MenuItem
-                            cursor="pointer"
-                            onClick={() => {
-                                handleSelection(locale);
-                            }}
-                            key={locale}>
-                            {/* <Image
-                                boxSize="2rem"
-                                borderRadius="full"
-                                src={`https://www.countryflags.io/${
-                                    locale == 'en' ? 'gb' : locale
-                                }/flat/64.png`}
-                                alt={locale}
-                                mr="12px"
-                            /> */}
-                            {getFlagEmoji(locale == 'en' ? 'gb' : locale)} {localeName}
-                        </MenuItem>
+                        <LinkBox as={MenuItem} cursor="pointer" key={locale}>
+                            <LinkOverlay
+                                as={Link}
+                                w="full"
+                                d="block"
+                                href={`/${locale !== 'en' ? locale : ''}`}
+                                alt={`choose ${locale}`}>
+                                {getFlagEmoji(locale === 'en' ? 'gb' : locale)}
+                                {localeName}
+                            </LinkOverlay>
+                        </LinkBox>
                     );
                 })}
             </MenuList>
--- cypress/integration/homepage/locales.spec.ts
+++ cypress/integration/homepage/locales.spec.ts
@@ -1,20 +1,68 @@
+import { en, fr, es, gr } from '../../../content/locale/';
+
 describe('Locale', () => {
-    it('Set locale to EN', () => {
-        cy.visit('/en');
-        cy.get('h2').contains('Welcome').screenshot();
+    const locales = [
+        { code: 'en', name: 'English' },
+        { code: 'fr', name: 'Français' },
+        { code: 'es', name: 'Español' },
+        { code: 'gr', name: 'Ελληνικά' }
+    ];
+    const translations = { en, fr, es, gr };
+    const defaultLocale = 'en';
+
+    beforeEach(() => {
+        cy.visit('/');
     });
+
+    locales.forEach((locale) => {
+        it(`Set locale to ${locale.name} (${locale.code})`, () => {
+            cy.get('button[e2e=localeToggle]').should('be.visible').screenshot().click();
+            cy.get('[e2e=localeMenu]')
+                .screenshot()
+                .should('be.visible')
+                .contains(locale.name)
+                .should('exist')
+                .screenshot()
+                .click({ force: true });
+            cy.location('pathname').should(
+                'eq',
+                `/${locale.code !== defaultLocale ? locale.code : ''}`
+            );
+            cy.get('h2')
+                .first()
+                .contains(translations[locale.code]['/'].welcomeMessage)
+                .screenshot();
+        });
+    });
+    // it('Set locale to EN', () => {
+    //     cy.get('button[e2e=localeToggle]').click();
+    //     cy.get('[e2e=localeMenu]').should('be.visible').contains('English').click();
+    //     cy.get('h2').first().contains('Welcome').screenshot();
+    // });
     it('Set locale to FR', () => {
-        cy.visit('/fr');
-        cy.get('h2').contains('Bienvenue').screenshot();
+        cy.get('button[e2e=localeToggle]').should('be.visible').screenshot().click();
+        cy.get('[e2e=localeMenu]')
+            .screenshot()
+            .should('be.visible')
+            .contains('Français')
+            .should('exist')
+            .screenshot()
+            .click({ force: true });
+        cy.location('pathname').should('eq', '/fr');
+        cy.get('h2').first().contains('Bienvenue').screenshot();
     });
-    it('Set locale to ES', () => {
-        cy.visit('/es');
-        cy.get('h2').contains('Bienvenido').screenshot();
-    });
-    it('Set locale to GR', () => {
-        cy.visit('/gr');
-        cy.get('h2').contains('καλώς').screenshot();
-    });
+    // it('Set locale to ES', () => {
+    //     cy.get('button[e2e=localeToggle]').click();
+    //     cy.get('[e2e=localeMenu]').should('be.visible').contains('Español').click();
+    //     // cy.location().should((loc) => expect(loc.pathname).to.eq('/es'));
+    //     cy.get('h2').first().contains('Bienvenido').screenshot();
+    // });
+    // it('Set locale to GR', () => {
+    //     cy.get('button[e2e=localeToggle]').click();
+    //     cy.get('[e2e=localeMenu]').should('be.visible').contains('Ελληνικά').click();
+    //     // cy.location().should((loc) => expect(loc.pathname).to.eq('/gr'));
+    //     cy.get('h2').first().contains('καλώς').screenshot();
+    // });
 });
 
 export {};
