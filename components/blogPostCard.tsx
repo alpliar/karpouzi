84b1b1b5c707077e3587f5535e445bf3cabe4c03
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-11-01 21:52:02 +0100

    fix: author avatar

--- components/blogPostCard.tsx
+++ components/blogPostCard.tsx
@@ -13,19 +13,17 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Card from './card';
 
-const karpouziAvatar = require('../public/icon-48x48.png');
-
 const BlogPostCard = ({ title, date, slug, author, authorAvatar }) => {
     return (
         <LinkBox>
             <Card>
                 <Box
                     maxH={64}
-                    bg={'gray.100'}
+                    bg="gray.100"
                     mt={-6}
                     mx={-6}
                     mb={6}
-                    pos={'relative'}
+                    pos="relative"
                     overflow="hidden">
                     <Img
                         src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
@@ -34,18 +32,18 @@ const BlogPostCard = ({ title, date, slug, author, authorAvatar }) => {
                 </Box>
                 <Stack>
                     <Text
-                        color={'green.500'}
-                        textTransform={'uppercase'}
-                        fontWeight="800"
+                        color="green.500"
+                        textTransform="uppercase"
+                        fontWeight="bold"
                         fontSize="sm"
-                        letterSpacing="1.1">
+                        letterSpacing={1}>
                         Blog
                     </Text>
                     <Stack h={32}>
                         <Heading
                             color={useColorModeValue('gray.700', 'white')}
-                            fontSize={'xl'}
-                            fontFamily={'body'}>
+                            fontSize="xl"
+                            fontFamily="body">
                             <Link
                                 href={{
                                     pathname: '/blog/post/[slug]',
@@ -57,18 +55,18 @@ const BlogPostCard = ({ title, date, slug, author, authorAvatar }) => {
                                 </LinkOverlay>
                             </Link>
                         </Heading>
-                        <Text color={'gray.500'} noOfLines={3}>
+                        <Text color="gray.500" noOfLines={4}>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
                             eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
                             voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
                         </Text>
                     </Stack>
                 </Stack>
-                <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
-                    <Avatar src={authorAvatar} alt={'Author'} bg="blackAlpha.400" />
-                    <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+                <Stack mt={6} direction="row" spacing={4} align="center">
+                    <Avatar src={authorAvatar} alt="Author" bg="green.500" />
+                    <Stack direction="column" spacing={0} fontSize="sm">
                         <Text fontWeight="600">{author}</Text>
-                        <Text color={'gray.500'}>{date} · 6min read</Text>
+                        <Text color="gray.500">{date} · 6min read</Text>
                     </Stack>
                 </Stack>
             </Card>
@@ -80,7 +78,7 @@ export default BlogPostCard;
 
 BlogPostCard.defaultProps = {
     author: 'Karpouzi',
-    authorAvatar: karpouziAvatar
+    authorAvatar: '/icon-48x48.png'
 };
 
 BlogPostCard.propTypes = {
