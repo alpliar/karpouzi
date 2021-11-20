e6c72191c5746b7ef560f5c4cf4209baee419eb9
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-11-20 22:31:14 +0100

    feature: allow rating icon to be customized

--- components/rating.tsx
+++ components/rating.tsx
@@ -1,20 +1,22 @@
-import { Text } from '@chakra-ui/react';
+import { GiWatermelon } from 'react-icons/gi';
 import { StarIcon } from '@chakra-ui/icons';
-
 import PropTypes from 'prop-types';
+import Icon from '@chakra-ui/icon';
+import { Text } from '@chakra-ui/layout';
 
 interface IOwnProps {
     rate: number;
     count: number;
+    icon: any;
 }
 
-const Rating = ({ rate, count }: IOwnProps) => {
+const Rating = ({ rate, count, icon = GiWatermelon }: IOwnProps) => {
     return (
         <Text>
             {Array(5)
                 .fill('')
                 .map((_, i) => (
-                    <StarIcon key={i} color={i < rate ? 'teal.500' : 'gray.300'} />
+                    <Icon as={icon} key={i} color={i < rate ? 'teal.500' : 'gray.300'} />
                 ))}
             <Text isTruncated as="span" ml="2" /*color="gray.600"*/ fontSize="sm">
                 {count.toString()} reviews
