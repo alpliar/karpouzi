cd2972ec8531d9c6be465929d08c63f5f2569f55
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-11-22 22:12:41 +0100

    style: add bg pattern to listing header

--- components/pageListingLayout.tsx
+++ components/pageListingLayout.tsx
@@ -1,4 +1,4 @@
-import { useColorMode } from '@chakra-ui/color-mode';
+import { useColorMode, useColorModeValue } from '@chakra-ui/color-mode';
 import { Box, Container, Divider, Flex, Heading, Stack, Wrap } from '@chakra-ui/layout';
 import { chakra } from '@chakra-ui/system';
 import Head from 'next/head';
@@ -30,6 +30,9 @@ const PageListingLayout = ({
             : `linear(to-b, ${gradientStart}, green.900)`;
     };
 
+    const bgPatternFillColor = useColorModeValue('white', 'gray.800');
+    const bgPattern = `url("data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h20v20H0V0zm10 17a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm20 0a7 7 0 1 0 0-14 7 7 0 0 0 0 14zM10 37a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm10-17h20v20H20V20zm10 17a7 7 0 1 0 0-14 7 7 0 0 0 0 14z' fill='${bgPatternFillColor}' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`;
+
     return (
         <PageLayout>
             <Head>
@@ -38,9 +41,27 @@ const PageListingLayout = ({
                     {title} - {siteTitle}
                 </title>
             </Head>
+
+            {/* LISTING'S HEADER */}
             <Box
+                position="relative"
                 // bgColor={headerBgColor(colorMode)}
-                bgGradient={headerBgGradient(colorMode)}>
+                bgGradient={headerBgGradient(colorMode)}
+                _before={{
+                    content: `''`,
+                    display: 'block',
+                    backgroundImage: bgPattern,
+                    position: 'absolute',
+                    width: '100%',
+                    height: '50%',
+                    top: '50%',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    opacity: 0.2,
+                    '-webkit-mask-image':
+                        '-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,0)), to(rgba(0,0,0,1)))'
+                }}>
                 <Container p={4} maxW={{ md: '4xl', xl: '6xl' }}>
                     <Stack spacing={2}>
                         <Breadcrumb entries={breadcrumbs} />
