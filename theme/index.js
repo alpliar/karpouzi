e3d453959177334e9b9941de4fb143705b599a74
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-11-24 18:30:02 +0100

    style: new breakpoint for 4k screen

--- theme/index.js
+++ theme/index.js
@@ -1,5 +1,15 @@
 // theme/index.js
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em', // 1535px
+    '3xl': '3840px' // 4k screen
+});
 
 // Global style overrides
 import styles from './styles';
@@ -22,13 +32,14 @@ const overrides = {
     //         }
     //     }
     // }
-    styles
+    styles,
     // borders,
     // Other foundational style overrides go here
     // components: {
     // Button
     // Other components go here
     // }
+    breakpoints
 };
 
 export default extendTheme(overrides);
