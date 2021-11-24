bd9c68c9fa27e8f7af0523827fb4fd947f022d37
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-11-24 18:50:39 +0100

    style: use listing template for cart page

--- pages/shop/cart/index.tsx
+++ pages/shop/cart/index.tsx
@@ -4,59 +4,47 @@ import { useSelector } from 'react-redux';
 import Breadcrumb from '../../../components/breadcrumb';
 import CartItem from '../../../components/cartItem';
 import Layout from '../../../components/pageLayout';
+import PageListingLayout from '../../../components/pageListingLayout';
+import ShopStat from '../../../components/shopStat';
 import { RootState } from '../../../reducer';
 
 export default function CartPage() {
     const { cart } = useSelector((state: RootState) => state.client);
 
     return (
-        <Layout>
-            <Head>
-                <title>Shop - Cart</title>
-            </Head>
+        <PageListingLayout
+            title="Cart"
+            breadcrumbs={[
+                {
+                    text: 'Shop',
+                    link: '/shop',
+                    alt: 'go to shop home',
+                    isCurrentPage: false
+                },
+                { text: 'Cart', link: '', alt: '', isCurrentPage: true }
+            ]}
+            titleSlot={<ShopStat label="Items" number={cart.length} />}>
+            {cart.length > 0 && (
+                <SimpleGrid
+                    minChildWidth={{ base: 'full', md: '450px' }}
+                    spacingX="0.5em"
+                    spacingY="1em">
+                    {cart.map((item, index) => (
+                        <CartItem
+                            key={`${item.slug}-${index}`}
+                            title={item.slug}
+                            quantity={item.quantity}
+                        />
+                    ))}
+                </SimpleGrid>
+            )}
 
-            <Container px={4} py={4} maxW="4xl">
-                <Breadcrumb
-                    entries={[
-                        {
-                            text: 'Shop',
-                            link: '/shop',
-                            alt: 'go to shop home',
-                            isCurrentPage: false
-                        },
-                        { text: 'Cart', link: '', alt: '', isCurrentPage: true }
-                    ]}
-                />
-                <Heading size="xl" pr="20%">
-                    My cart
-                </Heading>
-            </Container>
-
-            <Divider w="100%" />
-
-            <Container py={4} px={{ base: 0, sm: 4 }} maxW="4xl">
-                {cart.length > 0 && (
-                    <SimpleGrid
-                        minChildWidth={{ base: 'full', md: '450px' }}
-                        spacingX="0.5em"
-                        spacingY="1em">
-                        {cart.map((item, index) => (
-                            <CartItem
-                                key={`${item.slug}-${index}`}
-                                title={item.slug}
-                                quantity={item.quantity}
-                            />
-                        ))}
-                    </SimpleGrid>
-                )}
-
-                {cart.length === 0 && (
-                    <Alert status="info">
-                        <AlertIcon />
-                        You don&rsquo;t have any product in your cart !
-                    </Alert>
-                )}
-            </Container>
-        </Layout>
+            {cart.length === 0 && (
+                <Alert status="info">
+                    <AlertIcon />
+                    You don&rsquo;t have any product in your cart !
+                </Alert>
+            )}
+        </PageListingLayout>
     );
 }
