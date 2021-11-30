121bb83ab20a283344fdaaeff4b2e451b3bfe3d4
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2021-11-30 20:52:18 +0100

    chore: add config to disable pwa in localhost (disabled)

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports =
     withPWA({
         pwa: {
             dest: 'public',
+            // disable: process.env.NODE_ENV === 'development',
             register: true,
             runtimeCaching,
             publicExcludes: ['!robots.txt']
