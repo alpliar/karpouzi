73ab40bc75229f095b84c48ef61e4d880576db97
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-02-18 14:02:36 +0100

    fix: remove unknown attribute

--- components/blogPostCard.tsx
+++ components/blogPostCard.tsx
@@ -26,10 +26,7 @@ const BlogPostCard = ({ title, date, slug, author, authorAvatar }) => {
                     mb={6}
                     pos="relative"
                     overflow="hidden">
-                    <Img
-                        src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-                        layout="fill"
-                    />
+                    <Img src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
                 </Box>
                 <Stack>
                     <Text
