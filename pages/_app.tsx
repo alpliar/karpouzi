c5039bbdbdb887fa1ba69680bd08e09db861c688
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-04 11:00:49 +0200

    chore: fix ts errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import PropTypes from 'prop-types';
+import type { AppProps } from 'next/app';
+import { NextRouter, useRouter } from 'next/router';
 import { IntlProvider } from 'react-intl';
 import * as locales from '../content/locale';
 import { wrapper } from '../store';
@@ -9,13 +8,16 @@ import '../styles/global.scss';
 import theme from '../theme';
 
 const App = ({ Component, pageProps }: AppProps) => {
-    const router = useRouter();
+    const router: NextRouter = useRouter();
     const { locale, defaultLocale, pathname } = router;
-    const localeCopy: string = locales[locale || defaultLocale];
-    const messages: Record<string, string> = { ...localeCopy[pathname], ...localeCopy['common'] };
+    const localeCopy = locales[locale as keyof typeof locales];
+    const messages: Record<string, string> = {
+        ...localeCopy[pathname as keyof typeof localeCopy],
+        ...localeCopy['common']
+    };
 
     return (
-        <IntlProvider locale={locale} defaultLocale={defaultLocale} messages={messages}>
+        <IntlProvider locale={locale || 'en'} defaultLocale={defaultLocale} messages={messages}>
             <ChakraProvider theme={theme} resetCSS>
                 <Component {...pageProps} />
             </ChakraProvider>
@@ -24,8 +26,3 @@ const App = ({ Component, pageProps }: AppProps) => {
 };
 
 export default wrapper.withRedux(App);
-
-App.propTypes = {
-    Component: PropTypes.func,
-    pageProps: PropTypes.object
-};
