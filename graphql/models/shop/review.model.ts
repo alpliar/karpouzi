c42e45aa078e75a60ae6469def34c7d7a436fe0a
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-06 16:17:17 +0200

    chore: create reviews model

---
+++ graphql/models/shop/review.model.ts
@@ -0,0 +1,18 @@
+import Person from '../common/person.model';
+import { Id } from '../common/types.model';
+
+export interface ReviewExcerpt {
+    id: Id;
+    message?: string;
+    rating?: number;
+    isVerified?: boolean;
+    author?: Person;
+}
+
+export default interface Review {
+    id: Id;
+    message: string;
+    rating: number;
+    isVerified: boolean;
+    author: Person;
+}
