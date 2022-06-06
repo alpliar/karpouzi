734869284ff7b51809b18d1440ca9bdd8130d58b
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-06 17:58:40 +0200

    refactor: use new api for product page

--- pages/shop/product/[slug].tsx
+++ pages/shop/product/[slug].tsx
@@ -2,15 +2,14 @@ import { BellIcon } from '@chakra-ui/icons';
 import { Img } from '@chakra-ui/image';
 import { Badge, Box, Container, Divider, SimpleGrid, Text } from '@chakra-ui/layout';
 import axios from 'axios';
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import BlockQuote from '../../../components/blockQuote';
 import PageListingLayout from '../../../components/pageListingLayout';
 import Rating from '../../../components/rating';
 import { API_BASE_URL } from '../../../constants/api';
 import AddToCart from '../../../container/addToCart';
-import { getAllProductIds } from '../../../lib/products';
-import { sanitizeText } from '../../../utils/sanitize';
+import Product, { ShopProductsData } from '../../../graphql/models/shop/product.model';
 import { ProductResponse } from '../../api/shop/product/[slug]';
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
@@ -19,7 +18,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
         const {
             data: { product }
-        } = await axios.get<ProductResponse>(API_BASE_URL + '/shop/product' + slug);
+        } = await axios.get<ProductResponse>(API_BASE_URL + '/shop/product/' + slug);
 
         if (!product) throw new Error('Could not fetch category data');
 
@@ -29,6 +28,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
             }
         };
     } catch (err) {
+        // console.error(err);
         return {
             notFound: true
         };
@@ -36,24 +36,49 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const paths = await getAllProductIds();
+    try {
+        const {
+            data: { products }
+        } = await axios.post<ShopProductsData>(API_BASE_URL + '/shop/products');
 
-    return {
-        paths,
-        fallback: true
-    };
+        if (!products) throw new Error('Could not fetch products data');
+
+        return {
+            paths: products.map(({ slug }) => ({
+                params: {
+                    slug
+                },
+                locale: 'en'
+            })),
+            fallback: true
+        };
+    } catch (err) {
+        return {
+            paths: [],
+            fallback: true
+        };
+    }
 };
 
-const ProductPage: NextPage = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const ProductPage = ({ product }: { product: Product }) => {
     if (!product) return null;
 
-    const { title, slug, isNew, rating, imageUrl, reviewCount, contentHtml, price } = product;
+    // const { name, slug, isNew, rating, imageUrl, reviewCount, contentHtml, price } = product as Product;
     // if (!slug) {
     //     return false;
     // }
+
+    const isNew = false;
+    const reviewCount = product.reviews.length;
+    const rate =
+        reviewCount > 0
+            ? product.reviews.map((rev) => rev.rating).reduce((a, b) => a + b) / reviewCount
+            : 0;
+    const [firstPrice] = product.prices;
+
     return (
         <PageListingLayout
-            title={title}
+            title={product.name}
             breadcrumbs={[
                 {
                     text: 'Shop',
@@ -67,7 +92,12 @@ const ProductPage: NextPage = ({ product }: InferGetStaticPropsType<typeof getSt
                     alt: 'go to fruits category',
                     isCurrentPage: false
                 },
-                { text: slug, link: '', alt: '', isCurrentPage: true }
+                {
+                    text: product.name,
+                    link: `/shop/product/${product.slug}`,
+                    alt: `${product.name} picture`,
+                    isCurrentPage: true
+                }
             ]}
             titleSlot={
                 isNew && (
@@ -80,33 +110,42 @@ const ProductPage: NextPage = ({ product }: InferGetStaticPropsType<typeof getSt
             }
             introSlot={<BlockQuote noOfLines={3}>Blah blah</BlockQuote>}>
             <Head>
-                <title>Shop - {title}</title>
+                <title>Shop - {product.name}</title>
             </Head>
 
             <Container p={{ base: 0 }} maxW="full">
                 <SimpleGrid columns={{ base: 1, sm: 2 }} spacing={{ md: '1em' }}>
                     <Box bg="#282828" width="full" maxH="20em" overflow="hidden">
-                        <Img src={imageUrl} alt={`picture of ${title}`} width="full" />
+                        <Img
+                            src={product.picture.url}
+                            alt={`picture of ${product.name}`}
+                            width="full"
+                        />
                     </Box>
                     <Box bg="" p={4} textAlign={{ base: 'center', md: 'left' }}>
                         {/* <Heading>{title}</Heading> */}
 
-                        <Rating rate={parseInt(rating)} count={parseInt(reviewCount)} />
+                        <Rating rate={rate} count={reviewCount} />
 
                         <Text fontSize="4xl" fontWeight="bolder">
-                            {price}
+                            {firstPrice.amount}
+                            {` `}
+                            {firstPrice.currency}
                         </Text>
 
-                        <AddToCart slug={slug} quantity={1} />
+                        <AddToCart slug={product.slug} quantity={1} />
                     </Box>
                 </SimpleGrid>
 
                 <Box p={4} width="full" padding="1em">
                     <Divider my={4} w="100%" />
-                    <Box
+                    {/* <Box
                         className="externalHtml"
-                        dangerouslySetInnerHTML={{ __html: sanitizeText(contentHtml) }}
-                    />
+                        dangerouslySetInnerHTML={{ __html: sanitizeText(prod) }}
+                    /> */}
+                    <Box>
+                        <Text as="p">{product.description}</Text>
+                    </Box>
                 </Box>
             </Container>
         </PageListingLayout>
