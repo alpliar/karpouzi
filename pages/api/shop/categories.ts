b2c46bed85c4f4b63dcde87b31816dadae35dbad
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-06 09:54:33 +0200

    chore: rename file

--- pages/api/shop/categories.ts
+++ pages/api/shop/categories.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import apolloClient from '../../../graphql/apollo-client';
 import { ShopCategoriesData } from '../../../graphql/models/shop/category.model';
-import { GET_SHOP_CATEGORIES } from '../../../graphql/queries/product';
+import { GET_SHOP_CATEGORIES } from '../../../graphql/queries/shop';
 
 // interface CategoryProducts {
 //     count: number;
