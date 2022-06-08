328221d89fa9d5bbd010db35c2e979af34d3202e
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-09 00:26:43 +0200

    style: add loader placeholder to image component

--- components/image.tsx
+++ components/image.tsx
@@ -9,6 +9,23 @@ interface ImageProps {
     height: LayoutProps['height'];
 }
 
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="rgba(100,100,100,0.3)" offset="20%" />
+      <stop stop-color="rgba(100,100,100,0.5)" offset="50%" />
+      <stop stop-color="rgba(100,100,100,0.4)" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="transparent" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+    typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str);
+
 export const Image: React.FC<ImageProps & Omit<BoxProps, 'as'>> = ({
     src,
     alt,
@@ -18,7 +35,15 @@ export const Image: React.FC<ImageProps & Omit<BoxProps, 'as'>> = ({
 }) => {
     return (
         <Box position="relative" width={width} height={height} {...rest}>
-            <NextImage objectFit="cover" layout="fill" src={src} alt={alt} sizes="25vw 100vw" />
+            <NextImage
+                objectFit="cover"
+                layout="fill"
+                src={src}
+                alt={alt}
+                sizes="25vw 100vw"
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(500, 500))}`}
+                placeholder="blur"
+            />
         </Box>
     );
 };
