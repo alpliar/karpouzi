4998005502a907d78a414355572eb9cb0b122aac
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-08 23:38:52 +0200

    feature: api - new api route /api/shop/category/[slug]

---
+++ pages/api/shop/category/[slug].ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import apolloClient from '../../../../graphql/apollo-client';
+import {
+    ShopCategoryData,
+    ShopCategoryExcerpt
+} from '../../../../graphql/models/shop/category.model';
+import { GET_SHOP_CATEGORY } from '../../../../graphql/queries/shop/shop.categories.queries';
+
+export interface CategoryResponse {
+    category?: ShopCategoryExcerpt;
+    error?: string;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<CategoryResponse>) => {
+    try {
+        const { slug } = req.query;
+
+        const {
+            data: { category },
+            error
+        } = await apolloClient.query<ShopCategoryData>({
+            query: GET_SHOP_CATEGORY,
+            variables: { slug }
+        });
+
+        if (error) throw error;
+
+        res.status(200).json({
+            category
+        });
+    } catch (_err) {
+        console.log(_err);
+        res.status(200).json({
+            error: 'Could not fetch shop category'
+        });
+    }
+};
+
+export default handler;
