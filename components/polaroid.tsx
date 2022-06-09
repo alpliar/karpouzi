1a41765242c4f74c9c5eff3db8e8091b880cbbb7
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-09 11:04:44 +0200

    style: better colors on dark mode

--- components/polaroid.tsx
+++ components/polaroid.tsx
@@ -1,4 +1,11 @@
-import { Box, BoxProps, Text, TextProps } from '@chakra-ui/react';
+import {
+    Box,
+    BoxProps,
+    Text,
+    TextProps,
+    useColorModeValue,
+    BackgroundProps
+} from '@chakra-ui/react';
 
 interface IPolaroidProps {
     children: React.ReactNode;
@@ -7,18 +14,25 @@ interface IPolaroidProps {
 }
 
 const Polaroid: React.FC<IPolaroidProps> = ({ children, title, unstyled = false }) => {
+    const bgColor: BackgroundProps['backgroundColor'] = useColorModeValue('white', 'gray.700');
+    const textColor: TextProps['color'] = useColorModeValue('black', 'white');
+    const borderColor: BoxProps['border'] = '0.5px solid rgba(126,126,126,0.5)';
+
     const polaroidStyle: BoxProps = {
         padding: {
             base: '2rem',
             md: '1.5rem'
         },
-        backgroundColor: 'white',
+        backgroundColor: bgColor,
         boxShadow: '0 0.2rem 1.2rem rgba(0,0,0,0.2)',
         transform: {
             base: 'rotate(-1deg) scale(95%) translateY(0px)',
             lg: 'rotate(-1deg) scale(90%) translateY(-15%)'
         },
-        transition: 'all .25s ease'
+        transition: 'all .25s ease',
+        color: textColor,
+        border: borderColor,
+        rounded: 'lg'
     };
 
     const textStyle: TextProps = {
