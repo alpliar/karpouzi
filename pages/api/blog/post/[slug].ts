16a1c6423ee439142f0df18dadb1ef9d88f83ec3
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-10 22:17:17 +0200

    refactor: use helper in api page

--- pages/api/blog/post/[slug].ts
+++ pages/api/blog/post/[slug].ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import apolloClient from '../../../../graphql/apollo-client';
-import BlogPost, { BlogPostData } from '../../../../graphql/models/blog/post.model';
-import { GET_BLOG_POST } from '../../../../graphql/queries/blog/blog.posts.queries';
+import BlogPost from '../../../../graphql/models/blog/post.model';
+import PostHelper from '../../../../helpers/post.helper';
 
 export interface BlogPostResponse {
     post?: BlogPost;
@@ -12,18 +11,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<BlogPostRespons
     try {
         const { slug } = req.query;
 
-        const {
-            data: { post }
-        } = await apolloClient.query<BlogPostData>({
-            query: GET_BLOG_POST,
-            variables: { slug }
-        });
+        if (typeof slug !== 'string') throw new Error('Slug is missing in params');
 
-        if (post) {
-            res.status(200).json({
-                post
-            });
-        }
+        const post = await PostHelper.getPost(slug);
+
+        res.status(200).json({
+            post
+        });
     } catch (err) {
         res.status(404).json({
             error: `Could not fetch blog post`
