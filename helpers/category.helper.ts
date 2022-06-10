15e7968de1eb6df67d64e2259fe750afbbe6d82d
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-10 20:54:16 +0200

    feature: getCategories helper method

--- helpers/category.helper.ts
+++ helpers/category.helper.ts
@@ -1,9 +1,13 @@
 import apolloClient from '../graphql/apollo-client';
 import ShopCategory, {
+    ShopCategoriesData,
     ShopCategoryData,
     ShopCategoryWithProducts
 } from '../graphql/models/shop/category.model';
-import { GET_SHOP_CATEGORY } from '../graphql/queries/shop/shop.categories.queries';
+import {
+    GET_SHOP_CATEGORIES,
+    GET_SHOP_CATEGORY
+} from '../graphql/queries/shop/shop.categories.queries';
 import AssetHelper from './asset.helper';
 import ProductHelper from './product.helper';
 
@@ -28,6 +32,36 @@ export default class CategoryHelper {
                 };
 
                 resolve(newCategory);
+            } catch {
+                console.log('err - getCategory');
+                reject(undefined);
+            }
+        });
+    };
+
+    static getCategories = async (): Promise<Array<ShopCategoryWithProducts>> => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const {
+                    data: { categories }
+                } = await apolloClient.query<ShopCategoriesData>({
+                    query: GET_SHOP_CATEGORIES
+                });
+
+                const newCategories: Array<ShopCategoryWithProducts> = await Promise.all(
+                    categories.map(async (category) => {
+                        const productSlugs = category.products.map(({ slug }) => slug);
+                        const products = await ProductHelper.getProducts(productSlugs);
+
+                        return {
+                            ...category,
+                            picture: await AssetHelper.getAsset(category.picture.id),
+                            products
+                        };
+                    })
+                );
+
+                resolve(newCategories);
             } catch {
                 console.log('err - getCategory');
                 reject(undefined);
