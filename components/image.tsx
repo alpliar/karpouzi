84d67acaef488d7eff72ddceba26c912aab991ed
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-10 18:35:12 +0200

    refactor: remove default blurry placeholder

--- components/image.tsx
+++ components/image.tsx
@@ -13,23 +13,6 @@ interface ImageProps {
     blurDataURL?: NextImageProps['blurDataURL'];
 }
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="rgba(100,100,100,0.3)" offset="20%" />
-      <stop stop-color="rgba(100,100,100,0.5)" offset="50%" />
-      <stop stop-color="rgba(100,100,100,0.4)" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="transparent" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
-
-const toBase64 = (str: string) =>
-    typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str);
-
 export const Image: React.FC<ImageProps & Omit<BoxProps, 'as'>> = ({
     src,
     alt,
@@ -38,7 +21,7 @@ export const Image: React.FC<ImageProps & Omit<BoxProps, 'as'>> = ({
     sizes,
     priority = undefined,
     quality = 75,
-    blurDataURL = `data:image/svg+xml;base64,${toBase64(shimmer(500, 500))}`,
+    blurDataURL = undefined,
     ...rest
 }) => {
     return (
@@ -51,7 +34,7 @@ export const Image: React.FC<ImageProps & Omit<BoxProps, 'as'>> = ({
                 alt={alt}
                 sizes={sizes}
                 blurDataURL={blurDataURL}
-                placeholder="blur"
+                placeholder={blurDataURL ? 'blur' : undefined}
                 priority={priority}
                 quality={quality}
             />
