f9e8dd17cf70f5a7f999dc8a4ae236f12aaa5192
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-10 21:56:24 +0200

    refactor: use helper in api route

--- pages/api/blog/posts/index.ts
+++ pages/api/blog/posts/index.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import apolloClient from '../../../../graphql/apollo-client';
-import BlogPost, { BlogPostsData } from '../../../../graphql/models/blog/post.model';
-import { GET_BLOG_POSTS } from '../../../../graphql/queries/blog/blog.posts.queries';
+import BlogPost from '../../../../graphql/models/blog/post.model';
+import PostHelper from '../../../../helpers/post.helper';
 
 interface BlogPostsResponse {
     posts?: BlogPost[];
@@ -13,21 +12,16 @@ const handler = async (
     res: NextApiResponse<BlogPostsResponse | undefined>
 ) => {
     try {
-        const {
-            data: { posts }
-        } = await apolloClient.query<BlogPostsData>({
-            query: GET_BLOG_POSTS
-        });
+        const posts = await PostHelper.getPosts();
 
         if (posts) {
             res.status(200).json({
                 posts
             });
         }
-    } catch (err) {
-        // console.error(err);
+    } catch {
         res.status(404).json({
-            error: 'Could not fetch blogposts:'
+            error: 'Could not fetch blogposts'
         });
     }
 };
