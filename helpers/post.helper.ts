070c91fd4c7e1fb65f4a1347aaadeb3251f348df
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-10 21:56:11 +0200

    feature: write post helper class

---
+++ helpers/post.helper.ts
@@ -0,0 +1,62 @@
+import apolloClient from '../graphql/apollo-client';
+import BlogPost, { BlogPostData, BlogPostsData } from '../graphql/models/blog/post.model';
+import { Asset } from '../graphql/models/common/asset.model';
+import { GET_BLOG_POST, GET_BLOG_POSTS } from '../graphql/queries/blog/blog.posts.queries';
+import AssetHelper from './asset.helper';
+
+export default class PostHelper {
+    static getPost = async (slug: BlogPost['slug']): Promise<BlogPost> => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const {
+                    data: { post }
+                } = await apolloClient.query<BlogPostData>({
+                    query: GET_BLOG_POST,
+                    variables: {
+                        slug
+                    }
+                });
+
+                const newPost: BlogPost = {
+                    ...post,
+                    picture: await AssetHelper.getAsset(post.coverPicture.asset.id)
+                };
+
+                resolve(newPost);
+            } catch {
+                console.log('err - getPost');
+                reject(undefined);
+            }
+        });
+    };
+
+    static getPosts = async (): Promise<Array<BlogPost>> => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const {
+                    data: { posts }
+                } = await apolloClient.query<BlogPostsData>({
+                    query: GET_BLOG_POSTS
+                });
+
+                const newPosts: BlogPost[] = await Promise.all(
+                    posts.map(async (post) => {
+                        const asset: Asset = await AssetHelper.getAsset(post.coverPicture.asset.id);
+                        return {
+                            ...post,
+                            coverPicture: {
+                                ...post.coverPicture,
+                                asset
+                            }
+                        };
+                    })
+                );
+
+                resolve(newPosts);
+            } catch {
+                console.log('err - getPosts');
+                reject(undefined);
+            }
+        });
+    };
+}
