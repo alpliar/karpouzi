73dd38081e5f934e528292c67ec2b6ac85599385
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-14 23:39:51 +0200

    refactor: better error handling

--- pages/api/shop/categories.ts
+++ pages/api/shop/categories.ts
@@ -1,22 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ShopCategoryWithProducts } from '../../../graphql/models/shop/category.model';
 import CategoryHelper from '../../../helpers/category.helper';
+import errorHandler from '../../../utils/errorsHandler';
+
 export interface CategoriesResponse {
     categories?: ShopCategoryWithProducts[];
     error?: string;
 }
 
-const handler = async (
-    _req: NextApiRequest,
-    res: NextApiResponse<CategoriesResponse | undefined>
-) => {
+const handler = async (_req: NextApiRequest, res: NextApiResponse<CategoriesResponse>) => {
     try {
         const categories = await CategoryHelper.getCategories();
 
         res.status(200).json({
             categories
         });
-    } catch (_err) {
+    } catch (err) {
+        console.error(errorHandler(err));
         res.status(404).json({
             error: 'Could not fetch shop categories'
         });
