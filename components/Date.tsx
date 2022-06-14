8f7b2426ae01fbcb8f2a77922ce9484874d47930
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-15 00:49:23 +0200

    feature: translate dates

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,6 +1,22 @@
-import { format, parseISO } from 'date-fns';
+import { intlFormat, parseISO } from 'date-fns';
+import { useIntl } from 'react-intl';
 
 export default function Date({ dateString }: { dateString: string }) {
+    const { locale } = useIntl();
     const date = parseISO(dateString);
-    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+
+    return (
+        <time dateTime={dateString}>
+            {intlFormat(
+                date,
+                {
+                    weekday: 'short',
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                },
+                { locale }
+            )}
+        </time>
+    );
 }
