81c8ec850da29485d87929d3082466ccb78bc537
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-15 13:26:05 +0200

    chore: config new dynamic sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
     siteUrl: process.env.NEXT_PUBLIC_URL,
-    generateRobotsTxt: true
+    generateRobotsTxt: true,
+    exclude: ['/server-sitemap-index.xml'],
+    robotsTxtOptions: {
+        additionalSitemaps: [`${process.env.NEXT_PUBLIC_URL}/server-sitemap-index.xml`]
+    }
 };
 
 module.exports = config;
