d2a7f9c301aea0a1b8d7de11431366689787bae3
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-15 22:58:49 +0200

    chore: update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
     siteUrl: process.env.NEXT_PUBLIC_URL,
-    generateRobotsTxt: true,
-    exclude: ['/server-sitemap-index.xml']
-    // robotsTxtOptions: {
-    //     additionalSitemaps: [`${process.env.NEXT_PUBLIC_URL}/server-sitemap-index.xml`]
-    // }
+    generateRobotsTxt: true
 };
 
 module.exports = config;
