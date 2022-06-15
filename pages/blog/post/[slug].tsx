005ad300fabec23d30114116a139ba220a3e40be
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-15 11:25:56 +0200

    feature: use seo props for blog post page meta

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -3,6 +3,8 @@ import { Box, HStack, Stack, Text } from '@chakra-ui/layout';
 import { useBreakpointValue } from '@chakra-ui/react';
 import axios from 'axios';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { GiFountainPen, GiOpenBook } from 'react-icons/gi';
 import BlockQuote from '../../../components/blockQuote';
 import Date from '../../../components/Date';
@@ -60,10 +62,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 const BlogPostPage = ({ post }: { post: BlogPost }) => {
+    const { asPath } = useRouter();
     const pictureSizes = useBreakpointValue({ base: '100vw', md: '50vw' });
 
     if (!post) return null;
 
+    const authorName = post.authors[0].firstName;
     // const BlogBanner = chakra(Banner, {
     //     baseStyle: {
     //         maxWidth: '70ch'
@@ -71,54 +75,77 @@ const BlogPostPage = ({ post }: { post: BlogPost }) => {
     // });
 
     return (
-        <PageListingLayout
-            title={post.title}
-            breadcrumbs={[
-                {
-                    text: 'Blog',
-                    link: '/blog',
-                    alt: 'go back to blog home',
-                    isCurrentPage: false
-                },
-                { text: post.slug, link: '', alt: '', isCurrentPage: true }
-            ]}
-            titleSlot={
-                <Stack spacing={0}>
-                    <HStack>
-                        <Icon as={GiOpenBook} />
-                        <Text>{post.timeToRead} min read</Text>
-                    </HStack>
-                    <HStack>
-                        <Icon as={GiFountainPen} />
-                        <Date dateString={post.createdAt} />
-                    </HStack>
-                </Stack>
-            }
-            introSlot={
-                <BlockQuote noOfLines={3} author="Author">
-                    {post.subtitle}
-                </BlockQuote>
-            }
-            bannerSlot={<></>}>
-            <Stack>
-                <Image
-                    src={post.coverPicture.asset.url}
-                    alt={post.coverPicture.alternativeText}
-                    sizes={pictureSizes}
-                    priority
-                    width="full"
-                    height={{ base: '3xs', md: 'sm', xl: 'md' }}
-                    quality={75}
-                    blurDataURL={post.coverPicture.asset.thumbnail}
-                    objectFit="contain"
-                />
-                <Box>
-                    <Box maxW="70ch" fontSize={{ md: 'xl' }} margin="auto">
-                        {post.content}
+        <>
+            {post.meta && (
+                <Head>
+                    <title>{post.meta.title ? post.meta.title : post.title}</title>
+                    {post.meta.title && <meta property="og:title" content={post.meta.title} />}
+                    {post.meta.description && (
+                        <>
+                            <meta name="description" content={post.meta.description} />
+                            <meta property="og:description" content={post.meta.description} />
+                        </>
+                    )}
+                    {post.meta.keywords && (
+                        <meta property="keywords" content={post.meta.keywords.join(',')} />
+                    )}
+                    {post.meta.openGraphImage.url && (
+                        <meta property="og:image" content={post.meta.openGraphImage.url} />
+                    )}
+                    <meta property="og:url" content={process.env.NEXT_PUBLIC_URL + asPath} />
+                    <meta property="og:type" content="article" />
+                    <meta property="og:author" content={authorName} />
+                </Head>
+            )}
+            <PageListingLayout
+                title={post.title}
+                breadcrumbs={[
+                    {
+                        text: 'Blog',
+                        link: '/blog',
+                        alt: 'go back to blog home',
+                        isCurrentPage: false
+                    },
+                    { text: post.slug, link: '', alt: '', isCurrentPage: true }
+                ]}
+                titleSlot={
+                    <Stack spacing={0}>
+                        <HStack>
+                            <Icon as={GiOpenBook} />
+                            <Text>{post.timeToRead} min read</Text>
+                        </HStack>
+                        <HStack>
+                            <Icon as={GiFountainPen} />
+                            <Date dateString={post.createdAt} />
+                        </HStack>
+                    </Stack>
+                }
+                introSlot={
+                    <BlockQuote noOfLines={3} author="Author">
+                        {post.subtitle}
+                    </BlockQuote>
+                }
+                bannerSlot={<></>}>
+                <Stack>
+                    <Image
+                        src={post.coverPicture.asset.url}
+                        alt={post.coverPicture.alternativeText}
+                        sizes={pictureSizes}
+                        priority
+                        width="full"
+                        height={{ base: '3xs', md: 'sm', xl: 'md' }}
+                        quality={75}
+                        blurDataURL={post.coverPicture.asset.thumbnail}
+                        objectFit="contain"
+                    />
+                    <Box>
+                        <Box maxW="70ch" fontSize={{ md: 'xl' }} margin="auto">
+                            {post.content}
+                        </Box>
                     </Box>
-                </Box>
-            </Stack>
-        </PageListingLayout>
+                </Stack>
+            </PageListingLayout>
+        </>
     );
 };
 
