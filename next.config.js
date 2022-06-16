1ea4dfe7fe4a3f1aa6f6bef965db359adcf447d2
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-16 23:34:30 +0200

    feature: opt-in for faster compilation using swc

--- next.config.js
+++ next.config.js
@@ -29,7 +29,8 @@ module.exports =
                 removeConsole: {
                     exclude: ['error']
                 }
-            }
+            },
+            swcMinify: true
         })
     );
 // );
