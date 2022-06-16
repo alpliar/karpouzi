b16353c72c118f9bc2f3d4b992a88776785e4dc5
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-16 23:06:00 +0200

    perf: install & configure bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa');
 // const withImages = require('next-images');
 const runtimeCaching = require('next-pwa/cache');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+
 module.exports =
     // withImages(
-    withPWA({
-        pwa: {
-            dest: 'public',
-            // disable: process.env.NODE_ENV === 'development',
-            register: true,
-            runtimeCaching,
-            publicExcludes: ['!robots.txt']
-        },
-        images: {
-            domains: ['media.graphassets.com']
-        },
-        i18n: {
-            locales: ['en', 'fr', 'es', 'el'],
-            defaultLocale: 'en'
-        }
-    });
+    withBundleAnalyzer(
+        withPWA({
+            pwa: {
+                dest: 'public',
+                // disable: process.env.NODE_ENV === 'development',
+                register: true,
+                runtimeCaching,
+                publicExcludes: ['!robots.txt']
+            },
+            images: {
+                domains: ['media.graphassets.com']
+            },
+            i18n: {
+                locales: ['en', 'fr', 'es', 'el'],
+                defaultLocale: 'en'
+            }
+        })
+    );
 // );
