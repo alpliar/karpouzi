ea8bf699e89a276d7c369c9309e876b85c39280d
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-26 18:30:22 +0200

    tests: implement tests on shop reducer

--- __tests__/shop.reducer.test.ts
+++ __tests__/shop.reducer.test.ts
@@ -1,3 +1,4 @@
+import { addToCart, ADD_TO_CART, updateQuantityCart } from '../actions/shop';
 import reducer, { ShopState } from '../redux/reducer/shop';
 
 const initialState: ShopState = { cart: [] };
@@ -5,5 +6,37 @@ const initialState: ShopState = { cart: [] };
 describe('shop reducer', () => {
     it('should return the initial state', () => {
         expect(reducer(initialState, { type: '' })).toEqual(initialState);
+    });
+    it('should add a product to cart', () => {
+        expect(reducer(initialState, addToCart('test', 6))).toEqual({
+            ...initialState,
+            cart: [
+                {
+                    slug: 'test',
+                    quantity: 6
+                }
+            ]
+        });
+    });
+    it('should update quantity', () => {
+        const stateBefore = {
+            ...initialState,
+            cart: [
+                {
+                    slug: 'test',
+                    quantity: 6
+                }
+            ]
+        };
+        const stateAfter = {
+            cart: [
+                {
+                    slug: 'test',
+                    quantity: 8
+                }
+            ]
+        };
+
+        expect(reducer(stateBefore, updateQuantityCart('test', 8))).toEqual(stateAfter);
     });
 });
