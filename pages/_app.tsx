013055f240d6ee081cfe5b9d065171367daba9fa
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-27 22:29:39 +0200

    feature: use session provider in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { NextRouter, useRouter } from 'next/router';
 import { IntlProvider } from 'react-intl';
 import * as locales from '../content/locale';
@@ -7,7 +8,7 @@ import { wrapper } from '../redux/store';
 import '../styles/global.scss';
 import theme from '../theme';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
     const router: NextRouter = useRouter();
     const { locale, defaultLocale, pathname } = router;
     const localeCopy = locales[locale as keyof typeof locales];
@@ -17,11 +18,13 @@ const App = ({ Component, pageProps }: AppProps) => {
     };
 
     return (
-        <IntlProvider locale={locale || 'en'} defaultLocale={defaultLocale} messages={messages}>
-            <ChakraProvider theme={theme} resetCSS>
-                <Component {...pageProps} />
-            </ChakraProvider>
-        </IntlProvider>
+        <SessionProvider session={session}>
+            <IntlProvider locale={locale || 'en'} defaultLocale={defaultLocale} messages={messages}>
+                <ChakraProvider theme={theme} resetCSS>
+                    <Component {...pageProps} />
+                </ChakraProvider>
+            </IntlProvider>
+        </SessionProvider>
     );
 };
 
