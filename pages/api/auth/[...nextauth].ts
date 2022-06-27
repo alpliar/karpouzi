d207f02798f4436383896c29b1c75cd5bc03f1f3
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-27 23:12:40 +0200

    fix: incorrect env var names

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,12 +7,12 @@ export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         DiscordProvider({
-            clientId: process.env.DISCORD_ID || '',
-            clientSecret: process.env.DISCORD_SECRET || ''
+            clientId: process.env.DISCORD_CLIENT_ID || '',
+            clientSecret: process.env.DISCORD_CLIENT_SECRET || ''
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID || '',
-            clientSecret: process.env.GOOGLE_SECRET || ''
+            clientId: process.env.GOOGLE_CIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
         })
         // EmailProvider({
         //   server: {
