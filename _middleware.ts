60b8faed71b3e9c45ed4969f4ea0de0e4e3d4cec
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-02-19 17:35:08 +0100

    fix: build error

--- _middleware.ts
+++
@@ -1,9 +0,0 @@
-// import { NextRequest, NextResponse } from 'next/server';
-
-// export { default } from 'next-auth/middleware';
-
-// export const config = { matcher: ['/user/((?!login).*)'] };
-
-// export function middleware(request: NextRequest) {
-//     return NextResponse.redirect(new URL('/user/login', request.url));
-// }
