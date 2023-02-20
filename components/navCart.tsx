f154c6686799c112e252c29238ce9a54ba0d5bdc
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-02-20 12:11:30 +0100

    refactor: remove dead code

--- components/navCart.tsx
+++ components/navCart.tsx
@@ -1,46 +1,58 @@
-import { Badge, Box, Center } from '@chakra-ui/layout';
-import { useRouter } from 'next/dist/client/router';
-import { MouseEventHandler } from 'react';
+import { Badge, Box, Center, Text } from '@chakra-ui/layout';
+import { Icon } from '@chakra-ui/react';
 import { FaShoppingCart } from 'react-icons/fa';
-import NavButton from './navButton';
+import { useIntl } from 'react-intl';
+import Link from './link';
+import Popover from './Popover';
 
 interface IProps {
     cartCount: number;
 }
 
 const NavCart: React.FC<IProps> = ({ cartCount }) => {
-    const router = useRouter();
-    const href = '/shop/cart/';
-
-    const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
-        event.preventDefault();
-        router.push(href);
-    };
+    const { formatMessage } = useIntl();
+    const f = (id: string, values?: any) => formatMessage({ id }, values);
 
     return (
-        <Box position="relative">
-            <NavButton
-                e2e="cartCTA"
-                label="Go to cart"
-                icon={FaShoppingCart}
-                handleClick={handleClick}
-            />
-            {cartCount > 0 && (
-                <Center
-                    as={Badge}
-                    height={{ base: '4' }}
-                    width={{ base: '4' }}
-                    position="absolute"
-                    top="0"
-                    right="0"
-                    bgColor="red"
-                    borderRadius="full"
-                    fontSize={{ base: '0.7rem' }}
-                    variant="solid">
-                    {cartCount}
-                </Center>
-            )}
-        </Box>
+        <Popover
+            trigger={
+                <Box position="relative">
+                    {/* <NavButton
+                        e2e="cartCTA"
+                        label="Go to cart"
+                        icon={FaShoppingCart}
+                        handleClick={handleClick}
+                    /> */}
+                    <Link
+                        href="/shop/cart"
+                        color="currentColor"
+                        onClick={(event) => event.preventDefault()}
+                        asButton
+                        buttonProps={{
+                            variant: 'ghost',
+                            leftIcon: <Icon boxSize={5} as={FaShoppingCart} />
+                        }}>
+                        Cart
+                    </Link>
+                    {cartCount > 0 && (
+                        <Center
+                            as={Badge}
+                            height={{ base: '4' }}
+                            width={{ base: '4' }}
+                            position="absolute"
+                            top="0"
+                            right="0"
+                            bgColor="red"
+                            borderRadius="full"
+                            fontSize={{ base: '0.7rem' }}
+                            variant="solid">
+                            {cartCount}
+                        </Center>
+                    )}
+                </Box>
+            }>
+            {cartCount > 0 ? <Text>{f('accessCart')}</Text> : <Text>{f('cartIsEmpty')}</Text>}
+        </Popover>
     );
 };
 
