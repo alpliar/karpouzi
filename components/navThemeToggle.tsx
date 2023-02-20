681ec5dedad41947563fe00c7bffabcf86aef360
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-02-20 02:12:30 +0100

    feature: helper for theme toggle

--- components/navThemeToggle.tsx
+++ components/navThemeToggle.tsx
@@ -1,26 +1,42 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { useColorMode, useColorModeValue } from '@chakra-ui/color-mode';
-import NavButton from './navButton';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Box, Text } from '@chakra-ui/react';
 import { useIntl } from 'react-intl';
+import NavButton from './navButton';
+import Popover from './Popover';
 
 interface IProps {
     compact?: boolean;
 }
 
 const NavThemeToggle: React.FC<IProps> = ({ compact = true }) => {
+    const { colorMode } = useColorMode();
     const { formatMessage } = useIntl();
     const f = (id: string, values: any = null) => formatMessage({ id }, values);
 
     const { toggleColorMode: toggleMode } = useColorMode();
     const ToggleIcon = useColorModeValue(MoonIcon, SunIcon);
     return (
-        <NavButton
-            e2e="themeCTA"
-            icon={ToggleIcon}
-            label={f('theme')}
-            compact={compact}
-            handleClick={toggleMode}
-        />
+        <Popover
+            trigger={
+                <Box>
+                    <NavButton
+                        e2e="themeCTA"
+                        icon={ToggleIcon}
+                        label={f('theme')}
+                        compact={compact}
+                        handleClick={toggleMode}
+                    />
+                </Box>
+            }>
+            <>
+                <Text>
+                    {f('switchToThemeName', {
+                        name: f(colorMode === 'light' ? 'darkTheme' : 'lightTheme')
+                    })}
+                </Text>
+            </>
+        </Popover>
     );
 };
 
