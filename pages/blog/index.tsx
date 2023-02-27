6359102f32f7fda8bc00240aeaf0ff625487fc64
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-02-28 00:12:31 +0100

    style: harmonize blog header with shop header

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,10 +2,8 @@ import { Box } from '@chakra-ui/react';
 import axios from 'axios';
 import { GetStaticProps } from 'next';
 import { useIntl } from 'react-intl';
-import BlockQuote from '../../components/blockQuote';
 import LatestsPosts from '../../components/LatestsPosts';
 import PageListingLayout from '../../components/pageListingLayout';
-import ShopStat from '../../components/shopStat';
 import { API_BASE_URL } from '../../constants/api';
 import { ONE_HOUR } from '../../constants/time.constants';
 import { APP_MAX_WIDTH } from '../../constants/ui/main.layout';
@@ -41,6 +39,7 @@ const BlogHome = ({ posts }: { posts: Array<BlogPost> }) => {
         <PageListingLayout
             fullWidth
             title={f('title')}
+            titleComplement={`(${posts.length || 0} ${f('posts')})`.toLocaleLowerCase()}
             breadcrumbs={[
                 {
                     text: f('home'),
@@ -55,18 +54,11 @@ const BlogHome = ({ posts }: { posts: Array<BlogPost> }) => {
                     isCurrentPage: true
                 }
             ]}
-            titleSlot={
-                <ShopStat label={f('posts')} number={posts?.length ?? 0} textAlign="right" />
-            }
-            introSlot={
-                <BlockQuote noOfLines={3}>
-                    She put his pistol down, picked up her fletcher, dialed the barrel over to
+            introSlot={`She put his pistol down, picked up her fletcher, dialed the barrel over to
                     single shot, and very carefully put a toxin dart through the center of a broken
                     mirror bent and elongated as they fell. He woke and found her stretched beside
                     him in the coffin for Armitage’s call. Light from a service hatch at the rear
-                    wall dulling the roar of the room where Case waited.
-                </BlockQuote>
-            }>
+                    wall dulling the roar of the room where Case waited.`}>
             {posts && (
                 <Box mx="auto" maxWidth={APP_MAX_WIDTH} py={2}>
                     <LatestsPosts posts={posts} />
