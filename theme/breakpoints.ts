f7ab9fda3900c19d7daf82f7f813dd73a9e4bca7
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-03-02 10:02:36 +0100

    refactor: move breakpoints outta theme index

---
+++ theme/breakpoints.ts
@@ -0,0 +1,10 @@
+const breakpoints = {
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em', // 1535px
+    '3xl': '3840px' // 4k screen
+};
+
+export default breakpoints;
--- theme/index.ts
+++ theme/index.ts
@@ -1,4 +1,5 @@
 import { extendTheme } from '@chakra-ui/theme-utils';
+import breakpoints from './breakpoints';
 import fonts from './fonts';
 
 // Global style overrides
@@ -10,14 +11,7 @@ const theme = extendTheme({
         initialColorMode: 'system'
     },
     styles,
-    breakpoints: {
-        sm: '30em',
-        md: '48em',
-        lg: '62em',
-        xl: '80em',
-        '2xl': '96em', // 1535px
-        '3xl': '3840px' // 4k screen
-    },
+    breakpoints,
     components: {
         // LinkOverlay
     },
