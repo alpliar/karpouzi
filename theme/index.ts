9bc19371d97ab4c14abb50b20c318fbd86aa2112
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-03-02 01:34:40 +0100

    refactor: theme overrides

--- theme/index.ts
+++ theme/index.ts
@@ -1,45 +1,30 @@
-// theme/index.js
-import { extendTheme, ThemeOverride } from '@chakra-ui/theme-utils';
-import { ThemeConfig } from '@chakra-ui/theme';
+import { extendTheme } from '@chakra-ui/theme-utils';
+// import LinkOverlay from './components/linkOverlay';
 
 // Global style overrides
 import styles from './styles';
 
-const breakpoints = {
-    sm: '30em',
-    md: '48em',
-    lg: '62em',
-    xl: '80em',
-    '2xl': '96em', // 1535px
-    '3xl': '3840px' // 4k screen
-};
-
-// Foundational style overrides
-// import borders from './foundations/borders';
-
-// Component style overrides
-// import Button from './components/button';
-const config: ThemeConfig = {
-    initialColorMode: 'system'
-    // initialColorMode: 'dark'
-    // useSystemColorMode: true
-};
-
-const fonts: ThemeOverride['fonts'] = {
-    body: "'PT Sans Caption', sans-serif",
-    heading: "'Montserrat', sans-serif",
-    mono: undefined
-};
-
-// const overrides: ThemeOverride = {
-// Gives error : Type instantiation is excessively deep and possibly infinite.
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/42829
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const overrides: Record<string, any> = {
-    fonts,
-    config,
+const theme = extendTheme({
+    fonts: {
+        body: "'PT Sans Caption', sans-serif",
+        heading: "'Montserrat', sans-serif"
+        // mono: "",
+    },
+    config: {
+        initialColorMode: 'system'
+    },
     styles,
-    breakpoints
-};
+    breakpoints: {
+        sm: '30em',
+        md: '48em',
+        lg: '62em',
+        xl: '80em',
+        '2xl': '96em', // 1535px
+        '3xl': '3840px' // 4k screen
+    },
+    components: {
+        // LinkOverlay
+    }
+});
 
-export default extendTheme(overrides);
+export default theme;
