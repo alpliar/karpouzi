14b819c9f29979127a2b9932b768262f1fbe9e1a
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-03-08 17:10:16 +0100

    style: skewed sections

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -7,27 +7,27 @@ import {
     FlexProps,
     Heading,
     HeadingProps,
-    LayoutProps,
     Text,
     ThemingProps,
-    TransformProps,
-    useColorMode
+    useColorMode,
+    useColorModeValue
 } from '@chakra-ui/react';
 import React from 'react';
 import { APP_MAX_WIDTH } from '../../constants/ui/main.layout';
-import { BASE_TRANSITION } from '../../constants/ui/transitions';
+import { BASE_TRANSITION, VERY_SLOW_TRANSITION } from '../../constants/ui/transitions';
 import { getPattern, Pattern } from '../../utils/patterns';
-import ImageV2 from '../ImageV2';
+import ImageV2, { ImageV2Props } from '../ImageV2';
 import Link from '../link';
 
 export interface SectionProps {
     bgColor?: string;
     isEven?: boolean;
-    imageTransform?: TransformProps['transform'];
-    customImageSize?: LayoutProps['width'];
+    // imageTransform?: TransformProps['transform'];
+    // customImageSize?: LayoutProps['width'];
     customGap?: FlexProps['gap'];
     id?: string;
     useSecondaryColor?: boolean;
+    usePlainColor?: boolean;
     title: string;
     titleComplement?: React.ReactNode;
     subtitle?: string | React.ReactNode;
@@ -46,23 +46,30 @@ export interface SectionProps {
     paddingY?: BoxProps['padding'];
     centerItems?: boolean;
     isFirst?: boolean;
+    isLast?: boolean;
     customDirection?: FlexProps['direction'];
     customImageRatio?: AspectRatioProps['ratio'];
     priorityImage?: boolean;
     sectionPattern?: Pattern;
+    imageProps?: ImageV2Props['imageProps'];
+    imageContainerProps?: AspectRatioProps;
+    rest?: BoxProps;
+    skewed?: boolean;
+    fullWidth?: boolean;
 }
 
-const Section: React.FC<SectionProps> = ({
+const Section: React.FC<SectionProps & BoxProps> = ({
     id,
     pattern = 'kiwi',
     sectionPattern,
     bgColor = 'green',
     colorScheme = 'green',
     isEven = false,
-    imageTransform,
-    customImageSize,
+    // imageTransform,
+    // customImageSize,
     customGap,
     useSecondaryColor = false,
+    usePlainColor = false,
     title,
     titleComplement,
     subtitle,
@@ -75,58 +82,97 @@ const Section: React.FC<SectionProps> = ({
     component,
     headingTag = 'h2',
     headingFontSize = {
-        base: '2xl',
-        xl: '3xl'
+        base: '2xl'
+        // xl: '3xl'
     },
     aboveTitleSlot,
-    paddingY = { base: 4, sm: 8, md: 16, xl: 24 },
+    paddingY = { base: 8, md: 16, xl: 24 },
     centerItems = true,
     isFirst = false,
+    isLast = false,
     customDirection,
     customImageRatio,
-    priorityImage
+    priorityImage,
+    imageProps,
+    imageContainerProps,
+    skewed = true,
+    fullWidth = false,
+    ...rest
 }) => {
     const { colorMode } = useColorMode();
     const sectionShade =
         colorMode === 'light' ? (useSecondaryColor ? 50 : 100) : useSecondaryColor ? -1 : 800; // -1 to prevent colored background on dark mode
-    const sectionBgColor = `${colorScheme}.${sectionShade}`;
+    const sectionBgColor = usePlainColor
+        ? `${colorScheme}.${colorMode === 'light' ? 400 : 700}`
+        : `${colorScheme}.${sectionShade}`;
     const bannerShade = colorMode === 'light' ? 400 : 600;
     const bannerBgColor = `${colorScheme}.${bannerShade}` || bgColor;
     const buttonColorScheme = ['white', 'whiteAlpha', 'blackAlpha'].includes(colorScheme)
         ? 'gray'
         : colorScheme;
-    const gap = customGap || { base: 4, sm: 6, md: 12, xl: 24 };
+    // const gap = customGap || { base: 2, sm: 4, md: 12, xl: 24 };
+
+    const patternColor: string = useColorModeValue('black', 'white');
+    const patternOpacity: number = useColorModeValue(0.03, 0.05);
 
     const bgImage: BackgroundProps['backgroundImage'] = pattern
         ? getPattern(pattern, 'paper', 0.2)
         : undefined;
 
     const bgSection: BackgroundProps['backgroundImage'] = sectionPattern
-        ? getPattern(sectionPattern, 'textOnSurface', 0.04)
+        ? getPattern(sectionPattern, patternColor, patternOpacity)
         : undefined;
+
+    const transformAngle = '-1deg';
 
     return (
         <Box
+            position="relative"
+            isolation="isolate"
+            _before={{
+                content: 'no-open-quote',
+                position: 'absolute',
+                inset: 0,
+                transform: skewed ? { base: `skewY(${transformAngle})` } : undefined,
+                backgroundColor: sectionBgColor,
+                zIndex: -2
+            }}
+            _after={{
+                content: 'no-close-quote',
+                position: 'absolute',
+                inset: 0,
+                bgImage: `url("${bgSection}")`,
+                transform: skewed ? { base: `rotate(${transformAngle})` } : undefined,
+                zIndex: -1
+            }}
             id={id}
-            bgColor={sectionBgColor}
-            bgImage={`url("${bgSection}")`}
+            bgGradient={
+                isFirst
+                    ? `linear(to-tl, transparent, transparent,${sectionBgColor},${sectionBgColor})`
+                    : isLast
+                    ? `linear(to-br, transparent, transparent,${sectionBgColor},${sectionBgColor})`
+                    : undefined
+            }
             paddingBottom={paddingY}
-            paddingTop={isFirst ? 0 : paddingY}
-            transition={BASE_TRANSITION}
-            // paddingBottom={{ base: 4 }}
-        >
+            paddingTop={paddingY}
+            transition={VERY_SLOW_TRANSITION}
+            {...rest}>
             <Flex
                 transition={BASE_TRANSITION}
                 direction={customDirection || { base: 'column', sm: 'row' }}
-                w="full"
+                width="full"
                 paddingX={{ base: 2, sm: 4 }}
                 mx="auto"
-                maxW={APP_MAX_WIDTH}
+                maxWidth={fullWidth ? undefined : APP_MAX_WIDTH}
                 alignItems={centerItems ? 'center' : 'start'}
-                gap={gap}>
+                justify="space-between"
+                gap={customGap || { base: 2, sm: 12, md: 12, xl: 24 }}
+                rowGap={{ base: 4 }}>
                 <Box
-                    w="full"
                     flexGrow={1}
+                    paddingX={{ base: 2, sm: 0 }}
+                    justifyContent="center"
+                    width={{ base: 'full', sm: image ? '50%' : 'full' }}
                     order={{
                         base: 'initial',
                         sm: isEven ? 'initial' : 2
@@ -139,10 +185,9 @@ const Section: React.FC<SectionProps> = ({
                         alignItems="center"
                         gap={2}
                         as={headingTag}
-                        mb={description || component || url ? 4 : 0}
+                        mb={subtitle || description || component || url ? 4 : 0}
                         fontSize={headingFontSize}
                         letterSpacing="tight"
-                        // {...textColor}
                         lineHeight={{
                             md: 'shorter'
                         }}
@@ -167,12 +212,7 @@ const Section: React.FC<SectionProps> = ({
                     )}
 
                     {description && (
-                        <Text
-                            mb={5}
-                            // {...textColor}
-                            fontSize={{
-                                md: 'lg'
-                            }}>
+                        <Text mb={5} maxWidth="50ch">
                             {description}
                         </Text>
                     )}
@@ -184,6 +224,7 @@ const Section: React.FC<SectionProps> = ({
                             {component}
                         </Box>
                     )}
+
                     {url && (
                         <Box>
                             <Link
@@ -207,34 +248,26 @@ const Section: React.FC<SectionProps> = ({
 
                 {image && (
                     <Flex
+                        width={{ base: 'full', sm: '50%' }}
                         transition={BASE_TRANSITION}
-                        width={customImageSize || 'full'}
-                        // alignSelf="stretch"
-                        alignSelf={fillImage ? 'stretch' : 'start'}
-                        alignItems="start"
+                        alignSelf={fillImage ? 'stretch' : 'center'}
                         backgroundColor={bannerBgColor}
                         rounded="md"
                         bgImage={`url("${bgImage}")`}
-                        maxWidth={{ sm: 'xs' }}>
-                        <Flex
+                        maxWidth={imageContainerProps?.maxWidth || imageContainerProps?.maxW}>
+                        <ImageV2
+                            src={image}
+                            alt={title}
+                            blurDataURL={imageThumbnail}
+                            priority={priorityImage}
+                            ratio={customImageRatio || 1}
+                            imageProps={imageProps}
                             rounded="md"
-                            width="full"
-                            boxShadow="md"
+                            boxShadow="dark-lg"
                             overflow="hidden"
-                            transform={imageTransform}
-                            transition={BASE_TRANSITION}
                             alignSelf={fillImage ? 'stretch' : undefined}
-                            // maxWidth={{ sm: 'sm' }}
-                        >
-                            <ImageV2
-                                src={image}
-                                alt={title}
-                                blurDataURL={imageThumbnail}
-                                priority={priorityImage}
-                                ratio={customImageRatio || 1}
-                                width={{ base: 'full', md: 'sm' }}
-                            />
-                        </Flex>
+                            {...imageContainerProps}
+                        />
                     </Flex>
                 )}
             </Flex>
