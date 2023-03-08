39dbcbe1f3d6499b6cb0431a53a4282e0fee9a1e
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-03-08 17:13:44 +0100

    style: use section instead of banner for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import { Flex, Text } from '@chakra-ui/layout';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useIntl } from 'react-intl';
-import Banner from '../components/banner';
 import CallToActionNewsletter from '../components/callToActionNewsletter';
 import CallToActionWithAnnotation from '../components/callToActionWithAnnotation';
-import { SectionProps } from '../components/layout/Section';
+import Section, { SectionProps } from '../components/layout/Section';
 import SectionSideBySide from '../components/layout/sectionSideBySide';
 import PageLayout, { siteTitle } from '../components/pageLayout';
 
@@ -51,44 +50,52 @@ export default function Home() {
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <Banner pattern="wiggle" height="inherit">
-                <CallToActionWithAnnotation
-                    title={
-                        <>
-                            <Flex
-                                direction="column"
-                                as="span"
-                                align="start"
-                                justify="start"
-                                w="full">
-                                <Text
-                                    fontSize="0.75em"
-                                    style={{ fontVariantCaps: 'small-caps' }}
-                                    overflowWrap="anywhere">
-                                    {`${session?.user?.name ? session?.user?.name + ', ' : ''}
+            <Section
+                paddingY={{ base: 8, sm: 16, md: 20 }}
+                sectionPattern="wiggle"
+                title=""
+                usePlainColor
+                isFirst
+                height="inherit"
+                component={
+                    <CallToActionWithAnnotation
+                        title={
+                            <>
+                                <Flex
+                                    direction="column"
+                                    as="span"
+                                    align="start"
+                                    justify="start"
+                                    w="full">
+                                    <Text
+                                        fontSize="0.75em"
+                                        style={{ fontVariantCaps: 'small-caps' }}
+                                        overflowWrap="anywhere">
+                                        {`${session?.user?.name ? session?.user?.name + ', ' : ''}
                                     ${f('welcomeMessage')}`}
-                                </Text>
-                                <Text
-                                    fontSize="1.3em"
-                                    alignSelf="end"
-                                    lineHeight="1em"
-                                    as={'span'}
-                                    color="currentColor"
-                                    overflowWrap="anywhere"
-                                    whiteSpace="pre-wrap">
-                                    {f('commonSiteName')}
-                                </Text>
-                            </Flex>
-                        </>
-                    }
-                    description={<>{f('welcomeDescription')}</>}
-                    primaryActionLabel={f('welcomeDiscoverShop')}
-                    secondaryActionLabel={f('welcomeLearnMore')}
-                    primaryUrl="/shop"
-                    secondaryUrl="/blog"
-                    primaryActionAnnotation={f('welcomeDeals')}
-                />
-            </Banner>
+                                    </Text>
+                                    <Text
+                                        fontSize="1.3em"
+                                        alignSelf="end"
+                                        lineHeight="1em"
+                                        as={'span'}
+                                        color="currentColor"
+                                        overflowWrap="anywhere"
+                                        whiteSpace="pre-wrap">
+                                        {f('commonSiteName')}
+                                    </Text>
+                                </Flex>
+                            </>
+                        }
+                        description={<>{f('welcomeDescription')}</>}
+                        primaryActionLabel={f('welcomeDiscoverShop')}
+                        secondaryActionLabel={f('welcomeLearnMore')}
+                        primaryUrl="/shop"
+                        secondaryUrl="/blog"
+                        primaryActionAnnotation={f('welcomeDeals')}
+                    />
+                }
+            />
             <SectionSideBySide sections={sections} />
         </PageLayout>
     );
