bf4fcbc6f7e0d8408c316ca179bd857edcef4537
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-03-17 11:22:46 +0100

    refactor: implement rest prop

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,36 +1,33 @@
-import { Box, SpacerProps } from '@chakra-ui/react';
-import { PropsWithChildren } from 'react';
+import { Box, BoxProps, SpacerProps } from '@chakra-ui/react';
 import { BASE_TRANSITION } from '../constants/ui/transitions';
 
 interface ICardProps {
+    children: React.ReactNode;
     padding?: SpacerProps['p'];
     fullHeight?: boolean;
     hoverBg?: string;
+    rest?: BoxProps;
 }
 
 export const cardPadding = { base: 2, sm: 4, md: 6 };
 
-const Card: React.FC<PropsWithChildren<ICardProps>> = ({
-    children,
-    padding = cardPadding,
-    fullHeight = false,
-    hoverBg
-}) => {
+const Card: React.FC<ICardProps> = ({ children, fullHeight = false, hoverBg, ...rest }) => {
     return (
         <Box
-            w={'full'}
-            backgroundColor="surface"
-            boxShadow={'lg'}
-            rounded={{ base: 'md' }}
-            p={padding}
-            h={fullHeight ? '100%' : undefined}
-            overflow={'hidden'}
             _hover={{
                 boxShadow: 'xl',
                 bg: hoverBg
                 // transform: 'skewY(-0.1deg)'
             }}
-            transition={BASE_TRANSITION}>
+            backgroundColor="surface"
+            boxShadow="lg"
+            height={fullHeight ? '100%' : undefined}
+            overflow={'hidden'}
+            padding={cardPadding}
+            rounded="md"
+            transition={BASE_TRANSITION}
+            width="full"
+            {...rest}>
             {children}
         </Box>
     );
