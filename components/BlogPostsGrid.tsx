53d5096f5edeb295fe81f8152d5d91418a658335
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2023-03-17 02:12:42 +0100

    style: show more posts per row

--- components/BlogPostsGrid.tsx
+++ components/BlogPostsGrid.tsx
@@ -1,4 +1,6 @@
 import { Box, SimpleGrid } from '@chakra-ui/react';
+import { APP_MAX_WIDTH } from '../constants/ui/main.layout';
+import { BASE_TRANSITION } from '../constants/ui/transitions';
 import BlogPost from '../graphql/models/blog/post.model';
 import BlogPostCard from './BlogPostCard';
 
@@ -8,7 +10,14 @@ interface ILatestsPostsProps {
 
 const GridBlogPosts: React.FC<ILatestsPostsProps> = ({ posts }) => {
     return (
-        <SimpleGrid columns={{ base: 1, sm: 2, xl: 3, '3xl': 4 }} spacing={4}>
+        <SimpleGrid
+            transition={BASE_TRANSITION}
+            columns={{ base: 1, sm: 2, md: 3, xl: 4 }}
+            spacingX={{ base: 1, sm: 2, '2xl': 4 }}
+            spacingY={{ base: 4, sm: 4, '2xl': 8 }}
+            mx="auto"
+            maxWidth={APP_MAX_WIDTH}
+            py={{ base: 2, sm: 4 }}>
             {posts.map((post) => (
                 <Box p={0} key={post.id}>
                     <BlogPostCard post={post} />
